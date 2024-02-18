178e6054eb292a07e361e62cfc252681f985f162
Tomás R <tomaslopezraigal@gmail.com>
2024-02-18 12:03:24 +0100

    Replicating a part from Twitter

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,19 @@
+export function App () {
+    return (
+        <article>
+            <header>
+                <img alt="El avatar de Tomás" src="https://unavatar.io/tomasrl18"></img>
+                <div>
+                    <strong>Tomás Raigal López</strong>
+                    <span>@raigal_tomas</span>
+                </div>
+            </header>
+
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
