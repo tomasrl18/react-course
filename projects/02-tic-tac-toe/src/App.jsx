3c52b0bbeb8bc21f669550d7d1e3a799fe1c9563
Tomás R <tomaslopezraigal@gmail.com>
2024-02-21 15:58:11 +0100

    Detect winner

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -23,19 +23,57 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   )
 } 
 
+const WINNER_COMBINATIONS = [
+  [0, 1, 2], // top row
+  [3, 4, 5], // middle row
+  [6, 7, 8], // bottom row
+  [0, 3, 6], // left column
+  [1, 4, 7], // middle column
+  [2, 5, 8], // right column
+  [0, 4, 8], // diagonal 1
+  [2, 4, 6], // diagonal 2
+];
+
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
 
   const [turn, setTurn] = useState(TURNS.X);
 
+  // null = loser, false = draw
+  const [winner, setWinner] = useState(null);
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBINATIONS) {
+      const [a, b, c] = combo;
+
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[b] === boardToCheck[c]
+      ) {
+        return boardToCheck[a];
+      }
+      
+      return null;
+    }
+  }
 
   const updateBoard = (index) => {
+    if (board[index] || winner) return 
+    
     const newBoard = [...board];
     newBoard[index] = turn;
     setBoard(newBoard);
 
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+      alert(`Player ${newWinner} wins!`);
+    }
   }
 
   return (
