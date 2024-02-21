8362a5da59305bf9eee4ab8993cdb5882eb27f3a
Tomás R <tomaslopezraigal@gmail.com>
2024-02-21 13:05:53 +0100

    Rendering lists

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,30 +1,44 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 
+const users = [
+    {
+        userName: 'raigal_tomas',
+        isFollowing: true,
+        name: 'Tomás Raigal López'
+    },
+    {
+        userName: 'adrian_marin_96',
+        isFollowing: false,
+        name: 'Adrián Marín Chorro'
+
+    },
+    {
+        userName: 'FuenSansano',
+        isFollowing: true,
+        name: 'Fuensanta Sansano Montoya'
+    },
+    {
+        userName: 'marmol89',
+        isFollowing: false,
+        name: 'Jose Manuel Mármol Alfocea'
+    }
+]
+
 export function App () {
     return (
         <section className='App'>
-            <TwitterFollowCard
-                userName="raigal_tomas"
-                initialIsFollowing={true}
-            >
-                Tomás Raigal López
-            </TwitterFollowCard>
-            <TwitterFollowCard
-                userName="adrian_marin_96"
-            >
-                Adrián Marín Chorro
-            </TwitterFollowCard>
-            <TwitterFollowCard
-                userName="FuenSansano"
-            >
-                Fuensanta Sansano Montoya
-            </TwitterFollowCard>
-            <TwitterFollowCard
-                userName="marmol89"
-            >
-                Jose Manuel Mármol Alfocea
-            </TwitterFollowCard>
+            {
+                users.map(({userName, isFollowing, name}) => 
+                    <TwitterFollowCard
+                        key={userName}
+                        userName={userName}
+                        initialIsFollowing={isFollowing}
+                    >
+                        {name}
+                    </TwitterFollowCard>
+                )
+            }
         </section>
     )
 }
