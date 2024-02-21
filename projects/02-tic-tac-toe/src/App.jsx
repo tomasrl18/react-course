2c326bd0ab7966339802fff6eea5dab8a1908d96
Tomás R <tomaslopezraigal@gmail.com>
2024-02-21 17:06:05 +0100

    Saving the game in localStorage

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -9,9 +9,15 @@ import { TURNS } from './constants.js';
 import { checkWinner, checkEndGame } from './logic/board.js';
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board');
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  });
 
-  const [turn, setTurn] = useState(TURNS.X);
+  const [turn, setTurn] = useState(() => {
+    const turnsFromStorage = window.localStorage.getItem('turn');
+    return turnsFromStorage ?? TURNS.X;
+  });
 
   // null = loser, false = draw
   const [winner, setWinner] = useState(null);
@@ -20,6 +26,9 @@ function App() {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+
+    window.localStorage.removeItem('board');
+    window.localStorage.removeItem('turn');
   }
 
   const updateBoard = (index) => {
@@ -33,6 +42,9 @@ function App() {
 
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+
+    window.localStorage.setItem('board', JSON.stringify(newBoard));
+    window.localStorage.setItem('turn', newTurn);
 
     const newWinner = checkWinner(newBoard);
     if (newWinner) {
