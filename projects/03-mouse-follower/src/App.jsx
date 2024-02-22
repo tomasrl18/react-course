401a59aa1ff81d866ccbed8e3c6ba364376b97f2
Tomás R <tomaslopezraigal@gmail.com>
2024-02-22 10:47:32 +0100

    Create de ball and logic to follow de pointer

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,8 +1,43 @@
-
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    console.log('effect', { enabled });
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      console.log('handleMove', { clientX, clientY });
+      setPosition({ x: clientX, y: clientY });
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove);
+    }
+  }, [enabled]);
+
   return (
-    <h3>Project 3</h3>
+    <main>
+      <div style={{
+          position: 'absolute',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          border: '1px solid white',
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}0px)`,
+        }}
+      />
+      <button onClick={() => {setEnabled(!enabled)}}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </main>
   )
 }
 
