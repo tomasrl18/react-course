e8894f10f558c442798dc95debbf29279856c005
Tomás R <tomaslopezraigal@gmail.com>
2024-02-23 15:41:18 +0100

    Some changes

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -5,7 +5,6 @@ const FollowMouse = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
-
     const handleMove = (event) => {
       const { clientX, clientY } = event;
       setPosition({ x: clientX, y: clientY });
