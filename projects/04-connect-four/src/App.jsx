32a11679108f408e903bf908e2b55db9f5cf20c7
Tomás R <tomaslopezraigal@gmail.com>
2024-02-25 13:31:31 +0100

    Drawing the board

--- projects/04-connect-four/src/App.jsx
+++ projects/04-connect-four/src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const TURNS = {
+  RED: '🔴',
+  YELLOW: '🟡'
+}
 
+const board = Array(16).fill(null)
+const Square = ({ children, updateBoard, index }) => {
+  return (
+    <div className="square">
+      {children}
+    </div>
+  )
+} 
+
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className='board'>
+        <h1>Connect 4</h1>
+        <section className='game'>
+          {
+            board.map((_, index) => {
+              return (
+                <Square key={index} index={index}></Square>
+              )
+            })
+          }
+        </section>
+      </main>
     </>
   )
 }
