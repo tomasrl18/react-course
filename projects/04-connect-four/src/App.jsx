ec4b4dd3650b1651aa8eba7f6cc2abbb6d75db3a
Tomás R <tomaslopezraigal@gmail.com>
2024-02-25 14:14:24 +0100

    Detec winner

--- projects/04-connect-four/src/App.jsx
+++ projects/04-connect-four/src/App.jsx
@@ -6,6 +6,19 @@ const TURNS = {
   YELLOW: '🟡'
 }
 
+const WINNER_COMBINATIONS = [
+  [0, 1, 2, 3], // Firts row
+  [4, 5, 6, 7], // Second row
+  [8, 9, 10, 11], // Third row
+  [12, 13, 14, 15], // Fourth row
+  [0, 4, 8, 12], // First column
+  [1, 5, 9, 13], // Second column
+  [2, 6, 10, 14], // Third column
+  [3, 7, 11, 15], // Fourth column
+  [0, 5, 10, 15], // Diagonal from top-left to bottom-right
+  [3, 6, 9, 12] // Diagonal from top-right to bottom-left
+]
+
 const Square = ({ children, isSelected, updateBoard, index }) => {
   const className = `square ${isSelected ? 'is-selected' : ''}`;
 
@@ -25,13 +38,40 @@ function App() {
 
   const [turn, setTurn] = useState(TURNS.RED)
 
+  const [winner, setWinner] = useState(null);
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBINATIONS) {
+      const [a, b, c, d] = combo;
+
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c] &&
+        boardToCheck[a] === boardToCheck[d]
+      ) {
+        return boardToCheck[a];
+      }
+
+      return null;
+    }
+  }
+
   const updateBoard = (index) => {
+    if (board[index] || winner) return 
+
     const newBoard = [...board];
     newBoard[index] = turn;
     setBoard(newBoard);
     
     const newTurn = turn === TURNS.RED ? TURNS.YELLOW : TURNS.RED;
     setTurn(newTurn);
+
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+      alert(`Player ${newWinner} wins!`);
+    }
   }
 
   return (
