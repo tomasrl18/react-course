22f0aa51ec7285b52459fd5d8337258ffed28be4
Tomás R <tomaslopezraigal@gmail.com>
2024-02-25 13:54:17 +0100

    Draw RED or YELLOW depends on the turn

--- projects/04-connect-four/src/App.jsx
+++ projects/04-connect-four/src/App.jsx
@@ -10,7 +10,7 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   const className = `square ${isSelected ? 'is-selected' : ''}`;
 
   const handleClick = () => {
-    updateBoard()
+    updateBoard(index)
   };
 
   return (
@@ -25,7 +25,11 @@ function App() {
 
   const [turn, setTurn] = useState(TURNS.RED)
 
-  const updateBoard = () => {
+  const updateBoard = (index) => {
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+    
     const newTurn = turn === TURNS.RED ? TURNS.YELLOW : TURNS.RED;
     setTurn(newTurn);
   }
