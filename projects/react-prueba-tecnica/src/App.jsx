b6a1e4dcd1f87b15107d3aa69f398d1f888b927e
Tomás R <tomaslopezraigal@gmail.com>
2024-02-26 18:37:12 +0100

    Some styles

--- projects/react-prueba-tecnica/src/App.jsx
+++ projects/react-prueba-tecnica/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import './App.css'
 
 const CAT_ENDPOINT_RANDOM_FACT_URL = 'https://catfact.ninja/fact';
 //const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50?color=red&json=true`;
@@ -32,8 +33,10 @@ export function App() {
     return (
         <main>
             <h1>Kittys app</h1>
-            {fact && <p>{fact}</p>}
-            {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first three words for ${fact}`} />}
+            <section>
+                {fact && <p>{fact}</p>}
+                {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first three words for ${fact}`} />}
+            </section>
         </main>
     )
 }
