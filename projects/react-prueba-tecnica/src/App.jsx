770fc8cb569efee10327da1dcab12ad47d88cc68
Tomás R <tomaslopezraigal@gmail.com>
2024-02-26 17:22:29 +0100

    Show the response of the first API

--- projects/react-prueba-tecnica/src/App.jsx
+++ projects/react-prueba-tecnica/src/App.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react"
 
-const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50?color=red&json=true`;
+const CAT_ENDPOINT_RANDOM_FACT_URL = 'https://catfact.ninja/fact';
+//const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50?color=red&json=true`;
 
 export function App() {
-    const [fact, setFact] = useState('lorem ipsum dolor sit amet, consectetur adipiscing elit.')
+    const [fact, setFact] = useState()
 
     useEffect(() => {
-        fetch('https://catfact.ninja/fact')
+        fetch(CAT_ENDPOINT_RANDOM_FACT_URL)
             .then(res => res.json())
             .then(data => setFact(data.fact))
     }, [])
@@ -14,7 +15,7 @@ export function App() {
     return (
         <main>
             <h1>Kittys app</h1>
-            <p>{fact}</p>
+            {fact && <p>{fact}</p>}
         </main>
     )
 }
